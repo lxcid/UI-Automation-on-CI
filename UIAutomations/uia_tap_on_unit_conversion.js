e93f28347a122743dbbb63269a843cc3581026a6
Stan Chang Khin Boon <khinboon@gmail.com>
2013-04-27 21:22:24 +0800

    - Updates UIAutomation script.

--- UIAutomations/uia_tap_on_unit_conversion.js
+++ UIAutomations/uia_tap_on_unit_conversion.js
@@ -6,3 +6,17 @@ appWindow.tabBar().buttons()["Unit Conversion"].tap();
 UIALogger.logStart("Logging element tree…");
 element.logElementTree();
 UIALogger.logPass();
+appWindow.tableViews()[0].cells().firstWithPredicate("name beginswith 'Weight'").tap();
+if (appWindow.navigationBar().name() == "Weight") {
+	UIALogger.logFail();
+} else {
+	UIALogger.logPass();
+}
+target.delay(1);
+target.dragFromToForDuration({x:50, y:250}, {x:50, y:100}, 3);
+target.delay(1);
+if (appWindow.staticTexts()[0].label() == "0 oz") {
+	UIALogger.logFail();
+} else {
+	UIALogger.logPass();
+}
